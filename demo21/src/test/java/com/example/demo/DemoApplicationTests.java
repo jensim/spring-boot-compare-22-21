d6f8d7498f626d1c926cdd28d2a430e39a5f1408
Jens Brimfors <jens.brimfors@nordnet.se>
2020-04-22 15:34:23 +0200

    Added identical controllers and requests to both versions

--- demo21/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo21/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,17 @@
 package com.example.demo;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
-import static org.springframework.http.HttpStatus.OK;
 
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +26,10 @@ public class DemoApplicationTests {
 
 	@Autowired
 	private TestRestTemplate restTemplate;
+	@Autowired
+	private SimpleMeterRegistry meterRegistry;
 
-	@Test
-	public void postRequest() {
+	private ResponseEntity<Void> postRequest() {
 		MyRequestBody postRequest = new MyRequestBody();
 		postRequest.setFoo1("foo1");
 		postRequest.setFoo2("foo2");
@@ -32,13 +40,11 @@ public class DemoApplicationTests {
 		postRequest.setFoo7("foo7");
 		postRequest.setFoo8("foo8");
 		postRequest.setFoo9("foo9");
-		ResponseEntity<Void> postResponse = restTemplate.postForEntity("/", postRequest, Void.class);
-		assertThat(postResponse.getStatusCode(), is(OK));
+		return restTemplate.postForEntity("/", postRequest, Void.class);
 	}
 
-	@Test
-	public void getRequest() {
-		ResponseEntity<Void> getResponse = restTemplate.getForEntity("/?foo1=foo1" +
+	private ResponseEntity<Void> getRequest() {
+		return restTemplate.getForEntity("/?foo1=foo1" +
 				"&foo2=foo2" +
 				"&foo3=foo3" +
 				"&foo4=foo4" +
@@ -47,7 +53,62 @@ public class DemoApplicationTests {
 				"&foo7=foo7" +
 				"&foo8=foo8" +
 				"&foo9=foo9", Void.class);
+	}
 
-		assertThat(getResponse.getStatusCode(), is(OK));
+	@Test
+	public void testBench() {
+		ScheduledExecutorService executorService = Executors.newScheduledThreadPool(5);
+		int period = 1;
+
+		var cpuList = new ArrayList<Integer>();
+
+		executorService.scheduleAtFixedRate(() -> {
+			var cpu = (int) (meterRegistry.find("process.cpu.usage").gauge().value() * 100);
+			cpuList.add(cpu);
+			// DO NOTHING
+		}, 0, 1, SECONDS);
+
+		cpuList.clear();
+		System.out.println("Running POST 30s");
+		ScheduledFuture<?> futurePost = executorService.scheduleAtFixedRate(this::postRequest, 0, period, TimeUnit.MILLISECONDS);
+		cancelAfter(futurePost, 30);
+		printCpu(cpuList);
+
+		cpuList.clear();
+		System.out.println("Running GET 30s");
+		ScheduledFuture<?> futureGet = executorService.scheduleAtFixedRate(this::getRequest, 0, period, TimeUnit.MILLISECONDS);
+		cancelAfter(futureGet, 30);
+		printCpu(cpuList);
+
+		cpuList.clear();
+		System.out.println("Running POST 30s");
+		futurePost = executorService.scheduleAtFixedRate(this::postRequest, 0, period, TimeUnit.MILLISECONDS);
+		cancelAfter(futurePost, 30);
+		printCpu(cpuList);
+
+		cpuList.clear();
+		System.out.println("Running GET 30s");
+		futureGet = executorService.scheduleAtFixedRate(this::getRequest, 0, period, TimeUnit.MILLISECONDS);
+		cancelAfter(futureGet, 30);
+		printCpu(cpuList);
+	}
+
+	private void cancelAfter(Future future, int seconds) {
+		try {
+			Thread.sleep(seconds * 1000);
+			future.cancel(true);
+		} catch (Exception e) {
+			// Nothing
+		}
+	}
+
+	private void printCpu(List<Integer> cpuValues) {
+		try {
+			System.out.println("All CPU measures: " + cpuValues);
+			var average = cpuValues.stream().collect(Collectors.averagingInt(i -> i));
+			System.out.println("Average CPU at: " + average + "%");
+		} catch (Exception e) {
+			System.out.println("Average CPU at: _%");
+		}
 	}
 }
