d131bc24dd444190c47947a225b7ed3b12f1aa71
Jens Brimfors <jens.brimfors@nordnet.se>
2020-04-23 08:27:36 +0200

    Dropped the POST requests for less ambiguity

--- demo21/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo21/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -3,20 +3,16 @@ package com.example.demo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.OK;
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -27,27 +23,27 @@ public class DemoApplicationTests {
 
 	@Autowired
 	private TestRestTemplate restTemplate;
-	private final String postBody = "{ \"foo01\": \"foo1\", " +
-			"\"foo02\": \"ASC\", " +
-			"\"foo03\": 3, " +
-			"\"foo05\": 5.2, " +
-			"\"foo06\": 6, " +
-			"\"foo07\": \"DES\", " +
-			"\"foo08\": 8, " +
-			"\"foo09\": \"null\", " +
-			"\"foo11\": [\"VER\",\"HOR\",\"FAR\"] " +
-			"}";
-	private final String getUri = "/?foo01=foo1&foo02=ASC&foo03=3&foo05=5.2&foo06=6&foo07=DES&foo08=8&foo09=null" +
-			"&foo11=VER,HOR,FAR";
+	//language=TEXT
+	private final String getUri = "/" +
+			"?foo01=foo1" +
+			"&foo02=ASC" +
+			"&foo03=3" +
+			"&foo05=5.2" +
+			"&foo06=6" +
+			"&foo07=DES" +
+			"&foo08=8" +
+			"&foo09=null" +
+			"&foo11=VER,HOR,FAR" +
+			"&foo12=ASC" +
+			"&foo13=3" +
+			"&foo15=5.2" +
+			"&foo16=6" +
+			"&foo17=DES" +
+			"&foo18=8" +
+			"&foo19=null" +
+			"&foo21=VER,HOR,FAR,ASC,DES,NEA,VER,VER,HOR";
 	private RequestEntity<String> postEntity;
 	private final List<Integer> cpuList = new ArrayList<Integer>();
-
-	@Before
-	public void setUp() {
-		HttpHeaders headers = new HttpHeaders();
-		headers.add(HttpHeaders.CONTENT_TYPE, APPLICATION_JSON_VALUE);
-		postEntity = new RequestEntity<String>(postBody, headers, HttpMethod.POST, URI.create("/"), Void.TYPE);
-	}
 
 	@Test
 	public void testGet() {
@@ -56,8 +52,8 @@ public class DemoApplicationTests {
 	}
 
 	@Test
-	public void testPost() {
-		ResponseEntity<Void> request = restTemplate.exchange(postEntity, Void.class);
-		assertThat(request.getStatusCode(), is(OK));
+	public void testGetRequired() {
+		ResponseEntity<Void> request = restTemplate.getForEntity("/?foo01=foo01", Void.class);
+		assertThat(request.getStatusCode(), is(BAD_REQUEST));
 	}
 }
--- demo22/src/main/java/com/example/demo/MyController.java
+++ demo22/src/main/java/com/example/demo/MyController.java
@@ -1,14 +1,10 @@
 package com.example.demo;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-
 import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +13,6 @@ public class MyController {
 
     @GetMapping
     public void getter(
-
             @Schema(description = "foo01 description", example = "foo01")
             @RequestParam(required = true) String foo01,
             @Schema(description = "foo02 description", example = "ASC")
@@ -39,14 +34,29 @@ public class MyController {
             @Schema(description = "foo10 description", example = "DES,ASC,FAR")
             @RequestParam(required = false, defaultValue = "DES,ASC,FAR") List<SortOrder> foo10,
             @Schema(description = "foo11 description", example = "DES,ASC,FAR")
-            @RequestParam(required = true) List<SortOrder> foo11
+            @RequestParam(required = true) List<SortOrder> foo11,
+            @Schema(description = "foo12 description", example = "ASC")
+            @RequestParam(required = true) SortOrder foo12,
+            @Schema(description = "foo13 description", example = "3")
+            @Min(0) @Max(10) @RequestParam(required = true) int foo13,
+            @Schema(description = "foo14 description", example = "4")
+            @RequestParam(required = false) Integer foo14,
+            @Schema(description = "foo15 description", example = "5.5")
+            @Min(1) @Max(10) @RequestParam(required = true) double foo15,
+            @Schema(description = "foo16 description", example = "6")
+            @Min(1) @Max(10) @RequestParam(required = true) Long foo16,
+            @Schema(description = "foo17 description", example = "DES")
+            @RequestParam(required = true) SortOrder foo17,
+            @Schema(description = "foo18 description", example = "foo18")
+            @RequestParam(required = true) String foo18,
+            @Schema(description = "foo19 description", example = "foo19")
+            @RequestParam(required = true) String foo19,
+            @Schema(description = "foo20 description", example = "DES,ASC,FAR")
+            @RequestParam(required = false, defaultValue = "DES,ASC,FAR") List<SortOrder> foo20,
+            @Schema(description = "foo21 description", example = "DES,ASC,FAR")
+            @RequestParam(required = true) List<SortOrder> foo21
 
     ) {
-        // Do nothing
-    }
-
-    @PostMapping(consumes = {APPLICATION_JSON_VALUE})
-    public void poster(@RequestBody MyRequestBody body) {
         // Do nothing
     }
 }
